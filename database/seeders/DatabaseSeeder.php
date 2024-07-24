2d9b57968251e77e4969c631f108edeec9a4819a
mustafademir <mustafa10demir@gmail.com>
2024-07-25 01:11:58 +0300

    Product Create Migration|Model|Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call( [
             CategorySeeder::class,
             CustomerSeeder::class,
+            ProductSeeder::class,
         ] );
 
         User::factory()->create( [
